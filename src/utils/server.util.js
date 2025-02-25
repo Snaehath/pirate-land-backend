43c74798ad8291947b1915f0a4b196cc760b5378
Suraj Vijay <surajcpp115@gmail.com>
2025-02-25 22:57:03 +0530

    socket for room joining

--- src/utils/server.util.js
+++ src/utils/server.util.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 
-const origins = ["http://localhost:3000"];
+const origins = ["http://localhost:5173"];
 
 const io = new Server(httpServer, {
   cors: {
--- src/utils/socket.util.js
+++ src/utils/socket.util.js
@@ -1,46 +1,55 @@
+// custom
+const client = require("./astra-database.util");
+
+// add to sockets table
+const registerSocket = async (userId, socketId) => {
+  const QUERY = `INSERT INTO sockets(id, socket_id) VALUES (?, ?);`;
+  const VALUES = [userId, socketId];
+  try {
+      await client.execute(QUERY, VALUES, {prepare: true});
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+// remove from sockets table
+const unRegisterSocket = async (socketId) => {
+  const QUERY = `SELECT id FROM sockets WHERE socket_id = ?;`;
+  const VALUES = [socketId];
+  try {
+      const {rowLength, rows} = await client.execute(QUERY, VALUES, {prepare: true});
+      if (!rowLength) return;
+      const {id} = rows[0];
+      const QUERY1 = `DELETE FROM sockets WHERE id = ?;`;
+      const VALUES1 = [id];
+      await client.execute(QUERY1, VALUES1, {prepare: true});
+  } catch (err) {
+      console.log(err);
+  }
+};
+
 const socketHandler = (io) => {
   // user logs in
-  io.on("connection", (socket) => {
-    console.log(`[SERVER] ${socket.id} Connected`);
-
+  io.on("connection", socket => {
+    console.log(`[SOCKET] ${socket.id} Connected`);
+    const {userId} = socket.handshake.query;
+    registerSocket(userId, socket.id);
     // user leaves tab or logs out
     socket.on("disconnect", () => {
-      console.log(`[SERVER] ${socket.id} Disconnected`);
+      console.log(`[SOCKET] ${socket.id} Disconnected`);
+      unRegisterSocket(socket.id);
     });
     // user joins room
-    socket.on("joinRoom", (roomId) => {
+    socket.on("joinRoom", roomId => {
       socket.join(roomId);
-      console.log(`[SERVER] ${socket.id} joined ${roomId}`);
+      socket.broadcast.to(roomId).emit("playerJoined", {userId});
+      console.log(`[SOCKET] ${socket.id} joined ${roomId}`);
     });
     // user leaves room
-    socket.on("leaveRoom", (roomId) => {
+    socket.on("leaveRoom", roomId => {
       socket.leave(roomId);
-      console.log(`[SERVER] ${socket.id} left ${roomId}`);
-    });
-    // user updates room
-    socket.on("updateRoom", (roomId) => {
-      socket.broadcast.to(roomId).emit("updateRoom");
-      console.log(`[SERVER] ${socket.id} updates ${roomId}`);
-    });
-    // host launched game
-    socket.on("gameLaunched", (roomId) => {
-      socket.broadcast.to(roomId).emit("gameLaunched");
-      console.log(`[SERVER] ${socket.id} launched in ${roomId}`);
-    });
-    // user updates board
-    socket.on("updateBoard", (roomId) => {
-      socket.broadcast.to(roomId).emit("updateBoard", roomId);
-      console.log(`[SERVER] ${socket.id} updates ${roomId}`);
-    });
-    // user updates chance to all
-    socket.on("updateChance", (roomId) => {
-      socket.broadcast.to(roomId).emit("updateChance");
-      console.log(`[SERVER] ${socket.id} updates ${roomId}`);
-    });
-    // user updates every board
-    socket.on("updtBrd", (roomId) => {
-      socket.broadcast.to(roomId).emit("updtBrd");
-      console.log(`[SERVER] ${socket.id} updates ${roomId}`);
+      socket.broadcast.to(roomId).emit("playerLeft", {userId});
+      console.log(`[SOCKET] ${socket.id} left ${roomId}`);
     });
   });
 };
